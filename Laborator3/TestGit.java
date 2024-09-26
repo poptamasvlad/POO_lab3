b722d616057904e7bb446c07bb1db7127222571a
Poptamas Vlad <poptamasvlad@gmail.com>
2024-09-26 14:39:32 +0300

    Constructor explicit

--- Laborator3/TestGit.java
+++ Laborator3/TestGit.java
@@ -1,3 +1,10 @@
 public class TestGit {
-    
+    int numar;
+    char caracter;
+
+    public TestGit(int numar, char caracter)
+    {
+        this.numar = numar;
+        this.caracter = caracter;
+    }
 }
